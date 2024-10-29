6a9ad17b9da6b437d0cef1474fb09dff05f39a04
DCisHurt <roger33669@gmail.com>
2024-10-29 13:12:18 +0000

    add port logic

--- neuropico.py
+++ neuropico.py
@@ -1,8 +1,11 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, UART
 from micropython import const
 from .driver.ws2812c import WS2812C
 from .utilty.debounce import DebouncedInput
 from .motor import Motor
+from .port import AnalogPort, DigitalPort
+from .amplifier import Amplifier
+
 
 class NeuroPico:
 
@@ -22,16 +25,59 @@ class NeuroPico:
 
     PIN_MOTOR_EN = const(23)
 
+    PIN_CLK_IN = const(13)
+    PIN_CLK_nEN = const(12)
+
+    PIN_PORT1_A = const(17)
+    PIN_PORT1_B = const(26)
+    PIN_PORT2_A = const(18)
+    PIN_PORT2_B = const(27)
+    PIN_PORT3_A = const(21)
+    PIN_PORT3_B = const(28)
+    PIN_PORT4_A = const(22)
+    PIN_PORT4_B = const(29)
+
+    ADDR_AMP1 = const(0x18)
+    ADDR_AMP2 = const(0x4C)
+    ADDR_AMP3 = const(0x1A)
+    ADDR_AMP4 = const(0x4E)
+
+    PIN_PORT5_A = const(2)
+    PIN_PORT5_B = const(3)
+    PIN_PORT5_C = const(0)
+    PIN_PORT5_D = const(1)
+    PIN_PORT6_A = const(8)
+    PIN_PORT6_B = const(9)
+    PIN_PORT6_C = const(10)
+    PIN_PORT6_D = const(11)
+
+    PIN_LVL_SWITCH_5 = const(14)
+    PIN_LVL_SWITCH_6 = const(15)
+
     def __init__(self):
         self.LED = WS2812C(Pin(self.PIN_LED))
-        self.BTNA = DebouncedInput(self.PIN_BTNA, DebouncedInput.High)
-        self.BTNB = DebouncedInput(self.PIN_BTNB, DebouncedInput.High)
+        self.BTNA = DebouncedInput(self.PIN_BTNA, DebouncedInput.High, debounce_ms=20)
+        self.BTNB = DebouncedInput(self.PIN_BTNB, DebouncedInput.High, debounce_ms=20)
         self.I2C = I2C(id=0, scl=self.PIN_SCL, sda=self.PIN_SDA, freq=400_000)
-        self.MOTOR = Motor(pwm_pin_a=self.PIN_MOTOR_A, pwm_pin_b=self.PIN_MOTOR_B, enable_pin=self.PIN_MOTOR_EN, encoder_pin_a=self.PIN_ENC_A, encoder_pin_b=self.PIN_ENC_B)
-        # self.PORTA =
-        # self.PORTB =
-        # self.PORTC =
-        # self.PORTD =
-        # self.PORTE =
-        # self.PORTF =
-        # self.PORTG =
+        self.MOTOR = Motor(
+            pwm_pin_a=self.PIN_MOTOR_A,
+            pwm_pin_b=self.PIN_MOTOR_B,
+            enable_pin=self.PIN_MOTOR_EN,
+            encoder_pin_a=self.PIN_ENC_A,
+            encoder_pin_b=self.PIN_ENC_B,
+            i2c=self.I2C,
+        )
+        self.PORT1 = AnalogPort(self.PIN_PORT1_A, self.PIN_PORT1_B, Amplifier(self.I2C, self.ADDR_AMP1))
+        self.PORT2 = AnalogPort(self.PIN_PORT2_A, self.PIN_PORT2_B, Amplifier(self.I2C, self.ADDR_AMP2))
+        self.PORT3 = AnalogPort(self.PIN_PORT3_A, self.PIN_PORT3_B, Amplifier(self.I2C, self.ADDR_AMP3))
+        self.PORT4 = AnalogPort(self.PIN_PORT4_A, self.PIN_PORT4_B, Amplifier(self.I2C, self.ADDR_AMP4))
+        self.PORT5 = DigitalPort(
+            self.PIN_PORT5_A, self.PIN_PORT5_B, self.PIN_PORT5_C, self.PIN_PORT5_D, self.PIN_LVL_SWITCH_5, 0, 0
+        )
+        self.PORT6 = DigitalPort(
+            self.PIN_PORT6_A, self.PIN_PORT6_B, self.PIN_PORT6_C, self.PIN_PORT6_D, self.PIN_LVL_SWITCH_6, 1, 1
+        )
+        self.PORT7 = self.I2C
+
+        self.CLK_IN = UART(0)
+        self.CLK_nEN = Pin(self.PIN_CLK_nEN, Pin.OUT, 1)
--- port.py
+++ port.py
@@ -1,5 +1,92 @@
-from machine import Pin, Timer, PWM
-import time
+from machine import Pin, ADC, PWM, SPI, UART
+from micropython import const
+from .amplifier import Amplifier
 
 
-# class Port:
+class Port:
+    PIN_A = const(0)
+    PIN_B = const(1)
+    PIN_C = const(2)
+    PIN_D = const(3)
+
+    DIG = const(4)
+    ANG = const(5)
+    UART = const(6)
+    SPI = const(7)
+    I2C = const(8)
+
+    def __init__(self, pin_num_a, pin_num_b):
+        self._pin_list = [pin_num_a, pin_num_b]
+        self.mode = Port.DIG
+
+    def value(self, pin_index=1, logic=-1):
+        self._index_check(pin_index)
+
+        if logic == -1:
+            return self._read(pin_index)
+        else:
+            self._set(pin_index, logic)
+            return logic
+
+    def toggle(self, pin_index):
+        self._index_check(pin_index)
+
+        pin_temp = Pin(self._pin_list[pin_index], Pin.OUT)
+        pin_temp.toggle()
+
+    def _read(self, pin_index):
+        if self.mode == Port.DIG:
+            pin_temp = Pin(self._pin_list[pin_index], Pin.IN)
+            return pin_temp.value()
+        elif self.mode == Port.ANG:
+            pin_temp = ADC(self._pin_list[self.PIN_B])
+            return pin_temp.read_u16()
+
+    def _set(self, pin_index, logic):
+        if self.mode == Port.DIG:
+            pin_temp = Pin(self._pin_list[pin_index], Pin.OUT)
+        elif self.mode == Port.ANG:
+            pin_temp = Pin(self._pin_list[self.PIN_A], Pin.OUT)
+
+        pin_temp.value(logic)
+
+    def _index_check(self, index):
+        if index < 0 or index > len(self._pin_list):
+            raise ValueError("index out of range.")
+
+
+class AnalogPort(Port):
+    def __init__(self, pin_num_a, pin_num_b, amp: Amplifier, freq=100_000):
+        super().__init__(pin_num_a, pin_num_b)
+        self._amp = amp
+        self.freq = freq
+
+    def setGain(self, gain):
+        if gain <= 0:
+            raise ValueError("gain value cannot be negative.")
+        self._amp.setGain(gain)
+
+    def setPWM(self, duty):
+        if duty <= 0:
+            raise ValueError("duty rate cannot be negative.")
+        pwm = PWM(Pin(self._pin_list[Port.PIN_A]), freq=self.freq, duty_u16=0)
+        pwm.duty_u16(duty)
+
+
+class DigitalPort(Port):
+    LOGIC_3V3 = const(0)
+    LOGIC_5V = const(1)
+
+    def __init__(self, pin_num_a, pin_num_b, pin_num_c, pin_num_d, pin_level_switch, spi_id, uart_id):
+        super().__init__(pin_num_a, pin_num_b)
+        self._pin_list.append([pin_num_c, pin_num_d])
+        self._lvl_switch = Pin(pin_level_switch, Pin.OUT, 0)
+        self.spi = SPI(spi_id)
+        self.uart = UART(uart_id)
+        self.sck = self.cts = pin_num_a
+        self.mosi = self.rts = pin_num_b
+        self.miso = self.tx = pin_num_c
+        self.cs = self.rx = pin_num_d
+
+    def setLogicLevel(self, Level):
+        self._lvl_switch.value(Level)
