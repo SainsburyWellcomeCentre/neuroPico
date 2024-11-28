35b383db0035e6d8a78436b2230d7091642c5b86
DCisHurt <roger33669@gmail.com>
2024-11-28 18:19:51 +0000

    fix read angle raw

--- driver/as5600.py
+++ driver/as5600.py
@@ -3,7 +3,6 @@ import time
 from micropython import const
 
 ADDRESS = const(54)
-R_ANGLE_RAW = const(0x0C)
 R_ANGLE = const(0x0E)
 R_MAGNITUDE = const(0x1B)
 
@@ -14,16 +13,16 @@ class AS5600:
         self.i2c = i2c
 
     def read_angle(self):
-        return self._12_bit_decode(self._read_dat(R_ANGLE, 2))
+        return self.read_angle_raw() / 4096 * 360
 
     def read_angle_raw(self):
-        return self._12_bit_decode(self._read_dat(R_ANGLE_RAW, 2))
+        return self._12_bit_decode(self._read_dat(R_ANGLE, 2))
 
     def read_mag(self):
         return self._12_bit_decode(self._read_dat(R_MAGNITUDE, 2))
 
     def _12_bit_decode(self, buf):
-        return buf[0] * 256 + buf[1]
+        return (buf[0] & 0x0F) * 256 + buf[1]
 
     def _read_dat(self, mem_addr, length):
         return self.i2c.readfrom_mem(ADDRESS, mem_addr, length)
